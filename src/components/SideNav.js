c8246523f5c5b2cf65a430cb761878a7551f0569
Vishal-Marvel <vishal.pvn.edu@gmail.com>
2023-12-14 20:20:25 +0530

    Side Bar Added
    Signed-off-by: Vishal-Marvel <vishal.pvn.edu@gmail.com>

---
+++ src/components/SideNav.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger,} from "./ui/sheet.tsx"
+import {Button} from "./ui/button.tsx";
+import {Menu} from "lucide-react";
+
+const SideNav = () => {
+
+    return (
+        <Sheet>
+            <SheetTrigger>
+                <Button>
+                    <Menu/>
+                </Button>
+            </SheetTrigger>
+            <SheetContent side={"left"}>
+                <SheetHeader>
+                    <SheetTitle>Are you sure absolutely sure?</SheetTitle>
+                    <SheetDescription>
+                        This action cannot be undone. This will permanently delete your account
+                        and remove your data from our servers.
+                    </SheetDescription>
+                </SheetHeader>
+            </SheetContent>
+        </Sheet>
+
+    );
+};
+
+export default SideNav;
