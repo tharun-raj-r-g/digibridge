810e583be14bfc278ba12ae53d5ab7e34779a986
Vishal-Marvel <vishal.pvn.edu@gmail.com>
2023-12-17 16:20:37 +0530

    Added Page
    Signed-off-by: Vishal-Marvel <vishal.pvn.edu@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import {BrowserRouter, Route, Routes, useNavigate} from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import {ClerkProvider, RedirectToSignIn, SignedIn, SignedOut, SignIn, SignUp} from "@clerk/clerk-react";
 import Landing from "./pages/landing";
-
-// if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
-//     throw new Error("Missing Publishable Key")
-// }
+import Report from "./pages/Report";
+import Subject from "./pages/Subject";
+import Settings from "./pages/Settings";
+import Events from "./pages/Events";
+import Notes from "./pages/Notes";
+import Mentor from "./pages/Mentor";
 
 const clerkPubKey = "pk_test_YWRhcHRpbmctbW9uc3Rlci03MS5jbGVyay5hY2NvdW50cy5kZXYk";
 
@@ -18,16 +20,16 @@ function ClerkProviderWithRoutes() {
             navigate={(to) => navigate(to)}
         >
 
-            <div className="bg-[#eeeeee] h-[100vh]">
+            <div className="bg-[#eeeeee] h-[98%]">
                 <Routes>
                     <Route path="/" element={<Landing/>}/>
                     <Route
                         path="/sign-in/*"
-                        element={<SignIn routing="path" path="/sign-in"/>}
+                        element={<SignIn routing="path" path="/sign-in" afterSignInUrl={"/dashboard"}/>}
                     />
                     <Route
                         path="/sign-up/*"
-                        element={<SignUp routing="path" path="/sign-up"/>}
+                        element={<SignUp routing="path" path="/sign-up" afterSignUpUrl={"/dashboard"}/>}
                     />
                     <Route
                         path="/dashboard"
@@ -37,8 +39,122 @@ function ClerkProviderWithRoutes() {
                                     <div className={"absolute top-2"}>
                                         <SideNav/>
                                     </div>
-                                    <div className={"p-2 ml-20 mr-20 h-screen"}>
+                                    <div className={"p-2 ml-20 mr-20"}>
                                         <Dashboard/>
+                                    </div>
+
+                                </SignedIn>
+                                <SignedOut>
+                                    <RedirectToSignIn afterSignInUrl={"/dashboard"} afterSignUpUrl={"/signup"}/>
+                                </SignedOut>
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/report"
+                        element={
+                            <>
+                                <SignedIn>
+                                    <div className={"absolute top-2"}>
+                                        <SideNav/>
+                                    </div>
+                                    <div className={"p-2 ml-20 mr-20"}>
+                                        <Report/>
+                                    </div>
+
+                                </SignedIn>
+                                <SignedOut>
+                                    <RedirectToSignIn afterSignInUrl={"/dashboard"} afterSignUpUrl={"/signup"}/>
+                                </SignedOut>
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/notes"
+                        element={
+                            <>
+                                <SignedIn>
+                                    <div className={"absolute top-2"}>
+                                        <SideNav/>
+                                    </div>
+                                    <div className={"p-2 ml-20 mr-20"}>
+                                        <Notes/>
+                                    </div>
+
+                                </SignedIn>
+                                <SignedOut>
+                                    <RedirectToSignIn afterSignInUrl={"/dashboard"} afterSignUpUrl={"/signup"}/>
+                                </SignedOut>
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/events"
+                        element={
+                            <>
+                                <SignedIn>
+                                    <div className={"absolute top-2"}>
+                                        <SideNav/>
+                                    </div>
+                                    <div className={"p-2 ml-20 mr-20"}>
+                                        <Events/>
+                                    </div>
+
+                                </SignedIn>
+                                <SignedOut>
+                                    <RedirectToSignIn afterSignInUrl={"/dashboard"} afterSignUpUrl={"/signup"}/>
+                                </SignedOut>
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/settings"
+                        element={
+                            <>
+                                <SignedIn>
+                                    <div className={"absolute top-2"}>
+                                        <SideNav/>
+                                    </div>
+                                    <div className={"p-2 ml-20 mr-20"}>
+                                        <Settings/>
+                                    </div>
+
+                                </SignedIn>
+                                <SignedOut>
+                                    <RedirectToSignIn afterSignInUrl={"/dashboard"} afterSignUpUrl={"/signup"}/>
+                                </SignedOut>
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/subject"
+                        element={
+                            <>
+                                <SignedIn>
+                                    <div className={"absolute top-2"}>
+                                        <SideNav/>
+                                    </div>
+                                    <div className={"p-2 ml-20 mr-20"}>
+                                        <Subject/>
+                                    </div>
+
+                                </SignedIn>
+                                <SignedOut>
+                                    <RedirectToSignIn afterSignInUrl={"/dashboard"} afterSignUpUrl={"/signup"}/>
+                                </SignedOut>
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/mentor"
+                        element={
+                            <>
+                                <SignedIn>
+                                    <div className={"absolute top-2"}>
+                                        <SideNav/>
+                                    </div>
+                                    <div className={"p-2 ml-20 mr-20"}>
+                                        <Mentor/>
                                     </div>
 
                                 </SignedIn>
